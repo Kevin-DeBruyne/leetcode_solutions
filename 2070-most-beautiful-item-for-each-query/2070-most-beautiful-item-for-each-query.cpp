8815ccd08c889f05edb88763afa122a656d88f3d
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2022-06-23 13:07:52 +0530

    Time: 1040 ms (5.01%), Space: 97.8 MB (24.15%) - LeetHub

---
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        sort(items.begin(),items.end());
+        int n=items.size();
+        int pre=0;
+        
+        for(int i=0;i<n;i++){
+            pre=max(pre,items[i][1]);
+            items[i][1]=pre;
+        } 
+     for(auto i:items)
+            cout<<i[0]<<" "<<i[1]<<endl;
+        vector<int>ans;
+        for(int i=0;i<queries.size();i++){
+            int low=0;
+            int high=n-1;
+            int temp=0;
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                if(items[mid][0]<=queries[i]){
+                    temp=items[mid][1];
+                    low=mid+1;
+                }
+                else if(items[mid][0]>queries[i]){
+                    high=mid-1;
+                }
+                // else{
+                //     low=mid+1;
+                // }
+            }
+            ans.push_back(temp);
+        }
+    return ans;
+    }
+};
