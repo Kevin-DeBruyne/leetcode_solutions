f2df3405ddc1d189ebc28dab920cf972ea27dc72
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2022-07-21 22:56:15 +0530

    Time: 5 ms (78.23%), Space: 7.4 MB (76.48%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n=s.length();
-        int left=0;
-        int right=n-1;
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(iswalnum(s[i])){
-                count++;
-            }
-        }
-        if(count==0){
-            return true;
-        }
-        while(left<right){
-            if(!iswalnum(s[left])){
-                while(!iswalnum(s[left])){
-                    left++;
-            }
-            }
-            if(!iswalnum(s[right])){
-                while(!iswalnum(s[right])){
-                    right--;
-            }
-            }
-            if(tolower(s[left])!=tolower(s[right])){
-                return false;
-            }
-        left++;
-        right--;
-        }
-    return true;
-    }
+	int start=0, end=s.length()-1;
+	while(start<end) {
+		if (!isalnum(s[start])) start++;
+		else if (!isalnum(s[end])) end--;
+		else {
+			if (tolower(s[start++])!=tolower(s[end--])) return false;
+		}
+	}
+	return true;
+}
 };
