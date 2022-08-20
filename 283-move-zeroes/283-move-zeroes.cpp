97f2a94638a73c91ec6969255dddc0ebbab79145
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2022-08-20 09:44:18 +0530

    Time: 31 ms (72.57%), Space: 19.3 MB (13.44%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-	    int left=0,right=0,temp;
+	    int left=0,right=0;
         while(right<nums.size())
         {
             if(nums[right]==0)
-                ++right;
+                right++;
             else
             {
                 swap(nums[left], nums[right]);
-                ++left;
-                ++right;
+                left++;
+                right++;
             }
         } 
     }
