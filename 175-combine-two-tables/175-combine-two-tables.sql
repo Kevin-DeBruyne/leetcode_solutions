361dcbf4be3118e483c58fda5dd8e9ba64b6b2e5
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2022-08-24 10:43:53 +0530

    Time: 492 ms (49.45%), Space: 0B (100.00%) - LeetHub

---
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -0,0 +1,9 @@
+select firstName, lastName, city, state 
+from person natural left join address;
+# SELECT Customers.CustomerName, Orders.OrderID
+# FROM Customers
+# FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID
+# ORDER BY Customers.CustomerName;
+# SELECT FirstName, LastName, City, State
+# FROM Person
+# NATURAL LEFT JOIN Address;
