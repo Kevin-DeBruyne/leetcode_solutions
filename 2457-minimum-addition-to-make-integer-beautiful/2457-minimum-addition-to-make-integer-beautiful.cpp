3f4018cdb93aa4d67d61a7771fdf80e680ae1a94
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2022-11-29 20:00:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (53.66%) - LeetHub

---
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long digit_sum(long long n){
+        long long temp=0;
+        while(n!=0){
+            temp+=n%10;
+            n=n/10;
+        }
+        return temp;
+    }
+    long long makeIntegerBeautiful(long long n, int target) {
+        long long temp=n;
+        if(digit_sum(n)<=target){
+            return 0;
+        }
+        // n+=10-n%10;
+        long long i=100;
+        long long ans=0;
+        long long alt;
+        n+=10-(n%10);
+        
+        while(digit_sum(n)>target){
+            cout<<n<<endl;
+            // long long x=n;
+            alt=(n%i);
+            cout<<i-alt<<" ";
+            ans+=i-alt;
+            n+=i-alt;
+            i*=10;
+            
+        }
+        ans+=10-(temp%10);
+        return ans;
+    }
+};
