a4bcea8cdca53f32c344834fe650b778807b598d
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2022-12-08 23:35:40 +0530

    Time: 186 ms (42.34%), Space: 48.4 MB (48.65%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        
         int curr=0;
         int reach=nums[curr];
         curr++;
-        cout<<reach<<endl;
         while(curr<nums.size() && curr<=reach){
-            cout<<reach<<" ";
             reach=max(reach,nums[curr]+curr);
             curr++;
         }
-        cout<<endl;
         int n=nums.size();
-        cout<<curr<<endl;
         if(curr>=n){
             return true;
         }
