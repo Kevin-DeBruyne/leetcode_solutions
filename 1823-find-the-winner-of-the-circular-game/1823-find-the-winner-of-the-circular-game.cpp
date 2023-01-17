00234c4c8c99c0ec178f753a78e409e6008e001a
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2023-01-17 21:24:15 +0530

    Time: 3 ms (73.19%), Space: 5.9 MB (79.12%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    int findTheWinner(int n, int k) {
+    int helper(int n, int k){
         if(n==1){
-            return 1;
+            return 0;
         }
         else{
-            return ((findTheWinner(n-1, k)+k-1)%n)+1;
+            return (helper(n-1, k)+k)%n;
         }
+    }
+    int findTheWinner(int n, int k) {
+        return helper(n, k)+1;
     }
 };
