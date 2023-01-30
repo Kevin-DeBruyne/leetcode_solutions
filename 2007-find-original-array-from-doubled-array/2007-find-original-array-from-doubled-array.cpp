0f49b6085d320598821f92f0b1f975777f4419d1
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2023-01-30 23:14:25 +0530

    Time: 432 ms (80.21%), Space: 141.1 MB (80.63%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        
         int n=changed.size();
-        if(n==1){
+        if(n%2!=0){
             return {};
         }
-        unordered_map<int, int>mp;
         sort(changed.begin(), changed.end());
+        unordered_map<int, int>mp;
         for(auto x:changed){
             mp[x]++;
         }
         vector<int>vec;
         for(int i=0;i<n;i++){
-            // cout<<mp[changed[i]]<<endl;
             if(mp[changed[i]]==0){
-                // cout<<"if hu"<<endl;
-                // i++;
                 continue;
             }
             else{
-                // cout<<mp[changed[i]]<<" "<<mp[changed[i]*2]<<endl;
-            // cout<<i<<endl;
                 if(!mp.count(changed[i]*2)){
-                    // cout<<"kyu"<<endl;
                     return {};
                 }
                 else{
                     vec.push_back(changed[i]);
                     mp[changed[i]]--;
                     mp[changed[i]*2]--;
-                    if(mp[changed[i]]<0 ){
+                    if(mp[changed[i]]<0){
                         return {};
                     }
-                    if(mp[changed[i]]*2<0 ){
+                    if(mp[changed[i]*2]<0){
                         return {};
                     }
                 }
