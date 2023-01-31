b5f3e3e1cfa8ef18d14ad654da6e33de194507b8
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2023-01-31 10:57:28 +0530

    Time: 66 ms (94.31%), Space: 27.5 MB (82.34%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -1,8 +1,15 @@
+ int k1;
+     bool comp(vector<int>&a,vector<int>&b)
+    {
+      return a[k1]>b[k1];  
+    }
 class Solution {
 public:
-    
+   
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-        sort(score.begin(), score.end(), [&](vector<int>&a, vector<int>&b){return a[k]>b[k];});
+        k1=k;    
+        sort(score.begin(),score.end(),comp);
         return score;
+        
     }
 };
