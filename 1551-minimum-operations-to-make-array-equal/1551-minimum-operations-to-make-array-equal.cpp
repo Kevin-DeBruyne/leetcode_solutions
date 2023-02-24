c7488c930521800d02b84758834f6c4d279d81f5
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2023-02-24 23:43:08 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (93.21%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     int minOperations(int n) {
-        if(n%2!=0){
-            int maxi=2*(n-1)+1;
-            int mid=(maxi+1)/2;
-            int ans=0;
-            int i=0;
-            while(i<mid){
-                ans+=i;
-                i+=2;
-            }
-            return ans;
-        }
-        else{
-            int maxi=2*(n-1)+1;
-            int mid=(maxi+1)/2;
-            // cout<<mid<<endl;
-            int ans=0;
-            int i=1;
-            while(i<mid){
-                ans+=i;
-                i+=2;
-            }
-            return ans;
-        }
+        return n * n / 4;
     }
 };
