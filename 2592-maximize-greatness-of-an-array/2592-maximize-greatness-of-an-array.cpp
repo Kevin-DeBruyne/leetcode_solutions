27264326bf40bf8781617ee4cc8d709424cba79d
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2023-03-24 21:33:34 +0530

    Time: 323 ms (13.98%), Space: 112.4 MB (12.34%) - LeetHub

--- 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int maximizeGreatness(vector<int>& nums) {
-        vector<int>temp=nums;
-        sort(temp.begin(), temp.end());
+        multiset<int>s;
+        for(auto &x:nums){
+            s.insert(x);
+        }
         int ans=0;
-        int ind=0;
         for(int i=0;i<nums.size();i++){
-            auto ub=upper_bound(temp.begin()+ind+1, temp.end(), temp[i]);
-            if(ub!=temp.end()){
+            auto ub=s.upper_bound(nums[i]);
+            if(ub!=s.end()){
                 ans++;
-                ind=ub-temp.begin();
-                    
-                // temp.erase(temp.begin()+ind);
+                s.erase(ub);
+            }
+            else{
+                s.erase(s.begin());
             }
         }
         return ans;
