34ff47ae5dfb6955e223c9142ce37c61a5613d42
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2023-03-28 11:50:37 +0530

    Added solution - LeetHub

---
+++ Extreme nodes in alternate order - GFG/extreme-nodes-in-alternate-order.cpp
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+// } Driver Code Ends
+/*
+ A binary tree node has data, pointer to left child
+   and a pointer to right child 
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+
+class Solution{
+  public:
+    /* Function to print nodes of extreme corners
+    of each level in alternate order */
+    vector<int> ExtremeNodes(Node* root)
+    {
+        queue<Node*>q;
+        q.push(root);
+        vector<int>ans;
+        int lvl=0;
+        while(!q.empty()){
+            int n=q.size();
+            vector<int>temp;
+            lvl++;
+            for(int i=0;i<n;i++){
+                Node* n=q.front();
+                temp.push_back(n->data);
+                // cout<<n->data<<" ";
+                q.pop();
+                if(n->left){
+                    q.push(n->left);
+                }
+                if(n->right){
+                    q.push(n->right);
+                }
+            }
+            if(lvl%2==0){
+                ans.push_back(temp[0]);
+            }
+            else{
+                ans.push_back(temp[temp.size()-1]);
+            }
+            
+        }
+    return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+		Solution obj;
+        vector<int> nodes =  obj.ExtremeNodes(root);
+        for(int i = 0;i<nodes.size();i++){
+            cout<<nodes[i]<<" ";
+        }
+   		cout<<endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
