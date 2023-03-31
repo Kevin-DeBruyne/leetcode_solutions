d445fc0dd6241e126b0a7cfefe0b7e0c2b1c8118
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2023-03-31 13:50:25 +0530

    Time: 350 ms (56.55%), Space: 72.8 MB (55.20%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    long long MOD=1e9+7;
-    long long calcPower(long long base, long long power)  //modulo exponentation
-    {
-        long long ans = 1;
-        while(power )
-        {
-                ans = (ans * 2) % MOD; 
-                power--; 
-        }
-        return ans;
-    }
-    long long countWays(vector<vector<int>>& ranges) {
+    long long mod=1e9+7;
+    int countWays(vector<vector<int>>& ranges) {
         sort(ranges.begin(), ranges.end());
         int n=ranges.size();
         int temp=1;
-        long long ans=2;
-        int maxi=ranges[0][1];
+        int maxi=INT_MIN;
         for(int i=1;i<n;i++){
+            maxi=max(maxi, ranges[i-1][1]);
             if(ranges[i][0]<=maxi){
-                maxi=max(maxi, ranges[i][1]);
+                continue; 
             }
             else{
                 temp++;
-                maxi=ranges[i][1];
-                ans=(ans*2)%MOD;
             }
         }
-        // cout<<temp<<endl;
-        return ans;
+        long long ans=1;
+        for(int i=0;i<temp;i++){
+            ans=(ans*2)%mod;
+        }
+        return ans%mod;
     }
 };
