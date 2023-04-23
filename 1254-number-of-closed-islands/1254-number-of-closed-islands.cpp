7b70ef29a5a5c24d83808258b808748f1a850be2
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2023-04-23 22:11:27 +0530

    Time: 7 ms (98.65%), Space: 10.1 MB (28.72%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    void dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& vis, bool& yes){
-        int m=grid.size();
-        int n=grid[0].size();
-        if(i<0 || j<0 || i>=m || j>=n){
-            yes=false;
+    void dfs(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&vis,bool &bahar){
+        int M=grid.size();
+        int N=grid[0].size();
+        if(i<0 || j<0 || i>=M || j>=N){
+            bahar=true;
             return;
         }
-        if(grid[i][j]==1 || vis[i][j]==1){
+        if(vis[i][j]==1 || grid[i][j]==1){
             return;
         }
         vis[i][j]=1;
-        dfs(i+1, j , grid, vis, yes);
-        dfs(i, j+1 , grid, vis, yes);
-        dfs(i-1, j , grid, vis, yes);
-        dfs(i, j-1 , grid, vis, yes);
+        dfs(i+1, j, grid, vis,bahar);
+        dfs(i, j+1, grid, vis,bahar);
+        dfs(i-1, j, grid, vis,bahar);
+        dfs(i, j-1, grid, vis,bahar);
     }
     int closedIsland(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int cnt=0;
-        int n=grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int M=grid.size();
+        int N=grid[0].size();
+        vector<vector<int>> vis( M , vector<int> (N, 0)); 
+        int res=0;
+        for(int i=0;i<M;i++){
+            for(int j=0;j<N;j++){
+                bool bahar=false;
                 if(grid[i][j]==0 && vis[i][j]==0){
-                    bool yes=true;
-                    dfs(i, j , grid, vis, yes);
-                    if(yes){
-                        cnt++;
+                    dfs(i, j, grid, vis, bahar);
+                    if(!bahar){
+                        res++;
                     }
                 }
             }
         }
-        return cnt;
+        return res;
     }
+    
 };
