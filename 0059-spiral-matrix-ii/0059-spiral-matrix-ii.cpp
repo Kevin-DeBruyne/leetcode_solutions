757625b4bf0d1b171c349d1821c77bc84d0805d4
Kevin-DeBruyne <70618739+Kevin-DeBruyne@users.noreply.github.com>
2023-05-20 17:15:25 +0530

    Time: 5 ms (16.03%), Space: 6.6 MB (20.22%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -6,8 +6,8 @@ class Solution
             if(i<0 || j<0 || i>=n || j>=n || res[i][j]!=0){
                 return;
             }
-            cnt++;
             res[i][j]=cnt;
+            cnt++;
             if(goup) dfs(res, i-1 , j, cnt, true);
             dfs(res, i, j+1, cnt, false);
             dfs(res, i+1, j, cnt, false);
@@ -17,7 +17,7 @@ class Solution
         vector<vector < int>> generateMatrix(int n)
         {
             vector<vector<int>> res(n, vector<int> (n, 0));
-           	int cnt=0;
+           	int cnt=1;
             int i=0;
             int j=0;
             bool goup=false;
